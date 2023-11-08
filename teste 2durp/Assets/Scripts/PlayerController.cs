1dd108f4e37bb7de0e6e677fe98beb8bf208b138
JOYCE LIMA SOARES <20211170150069@ifrn.local>
2023-11-08 10:20:27 -0300

    animação pros lados

--- teste 2durp/Assets/Scripts/PlayerController.cs
+++ teste 2durp/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+public Animator playerAnimator;
+float input_x = 0;
+float input_y = 0;
+public  float speed = 2.5f;
+bool isWalking = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isWalking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_y = Input.GetAxisRaw("Vertical");
+        isWalking = (input_x != 0 || input_y != 0);
+
+        if (isWalking)
+        {
+            var move = new Vector3(input_x, input_y, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            playerAnimator.SetFloat("input_x", input_y);
+            playerAnimator.SetFloat("input_y", input_x);
+        }
+        playerAnimator.SetBool("isWalking", isWalking);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            playerAnimator.SetTrigger("attack");
+        }
     }
 }
